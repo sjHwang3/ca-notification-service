b1f2986cb4bb7d611f0811aecb16db6d82fe171b
sjhwang <sjhwang@carmerce.co.kr>
2023-07-18 10:21:18 +0900

    Initial commit

---
+++ src/main/java/com/carmerce/notf/CaNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carmerce.notf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carmerce/notf/CaNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmerce.notf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
